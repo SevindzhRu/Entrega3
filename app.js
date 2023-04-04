531e373d9a54184ae089f29e1442d05808e2bb12
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2023-04-03 20:19:01 -0500

    Entrega 3

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const producto = new ProductManager('./Productos.json')
 
 app.use(express.urlencoded({ extended: true }));
 
+
+app.get('/products', async(request, response)=>{
+    const prod = await producto.getProducts()
+    const limit = request.query.limit
+    if(!limit) return response.send(prod)
+    response.send(prod.slice(0,limit))
+})
+
+app.get('/products/:pid', async(request, response)=>{
+    const id = parseInt(request.params.pid)
+    const prod = await producto.getProductById(id)
+    if(!prod) return response.send({error: 'No se encuentra el producto'})
+    response.send(prod)
+})
+
 app.listen(8050, () => {
     console.log('Escuchandpo el puerto 8050')})
-
-    app.get('/products', async(request, response)=>{
-        const prod = await producto.getProducts()
-        const limit = request.query.limit
-        if(!limit) return response.send(prod)
-        response.send(prod.slice(0,limit))
-    })
-    
-    app.get('/products/:pid', async(request, response)=>{
-        const id = parseInt(request.params.pid)
-        const prod = await producto.getProductById(id)
-        if(!prod) return response.send({error: 'No se encuentra el producto'})
-        response.send(prod)
-    })
- 
